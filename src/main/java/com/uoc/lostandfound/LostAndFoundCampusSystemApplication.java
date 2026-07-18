5d15028325270bb7f85438eb1b8df606e273f944
chalaniChamodya <chalanichamodya2003@gmail.com>
2026-07-18 16:58:57 +0530

    Add Spring Boot project structure

---
+++ src/main/java/com/uoc/lostandfound/LostAndFoundCampusSystemApplication.java
@@ -0,0 +1,13 @@
+package com.uoc.lostandfound;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LostAndFoundCampusSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LostAndFoundCampusSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/uoc/lostandfound/LostAndFoundCampusSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uoc.lostandfound;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LostAndFoundCampusSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
