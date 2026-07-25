de6b7302c2800dc9e0b53f1d09e9d7b5d006b75d
AyandiRathnayake <2023s20127@stu.cmb.ac.lk>
2026-07-25 16:36:12 +0530

    Add files via upload

---
+++ src/main/java/com/uoc/lostandfound/repository/CampusLocationRepository.java
@@ -0,0 +1,8 @@
+package com.uoc.lostandfound.repository;
+
+import com.uoc.lostandfound.model.CampusLocation;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CampusLocationRepository extends JpaRepository<CampusLocation, Long> {
+
+}
