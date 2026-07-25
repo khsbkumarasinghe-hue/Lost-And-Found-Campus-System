0b1b6353b97b40268a723efc16f6e020bf3abae2
AyandiRathnayake <2023s20127@stu.cmb.ac.lk>
2026-07-25 20:44:53 +0530

    Delete src/main/java/com/uoc/lostandfound/repository/CampusLocationRepository.java

--- src/main/java/com/uoc/lostandfound/repository/CampusLocationRepository.java
+++
@@ -1,8 +0,0 @@
-package com.uoc.lostandfound.repository;
-
-import com.uoc.lostandfound.model.CampusLocation;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CampusLocationRepository extends JpaRepository<CampusLocation, Long> {
-
-}
